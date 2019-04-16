a3bc6228bed0c56abf0ddd4fbba70ef0e867e36c
albert <www.lvzhongdi@icloud.com>
2019-04-16 13:38:07 +0800

    finish test

--- app/src/main/java/com/lv/maven/demo/MainActivity.java
+++ app/src/main/java/com/lv/maven/demo/MainActivity.java
@@ -1,13 +1,13 @@
 package com.lv.maven.demo;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import com.lib.maven.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends BaseActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentView() {
+        return R.layout.activity_main;
     }
 }
